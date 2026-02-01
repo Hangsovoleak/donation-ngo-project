6e4117a11dbea4e103531ce5523420b266c03e36
Jupiter <hangsovoleakrorn@gmail.com>
2026-02-01 14:24:46 +0700

    changed port of backend server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import app from "./app.js";
 import prisma from "./db/prisma.js";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 async function warmUp() {
   try {
